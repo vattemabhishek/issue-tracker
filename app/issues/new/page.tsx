4d061b6508f144c1bc5880047636c332dafb68a6
vattemabhishek <vattemabhishek@gmail.com>
2024-04-26 14:08:18 +0530

    extract the error message

--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -10,6 +10,7 @@ import { useState } from 'react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { createIssueSchema } from '@/app/createIssueSchema'
 import {z} from 'zod';
+import ErrorMessage from '@/app/components/ErrorMessage'
 
 type IssueForm = z.infer<typeof createIssueSchema>
 
@@ -40,7 +41,7 @@ const NewIssuePage= () => {
         <TextField.Root>
           <TextField.Input placeholder='Title' {...register('title')} />
         </TextField.Root>
-        {errors.title && <Text color='red' as='p'>{errors.title.message}</Text>}
+      <ErrorMessage>{errors.title?.message}</ErrorMessage>
         <Controller
           name='description'
           control={control}
@@ -48,7 +49,7 @@ const NewIssuePage= () => {
             <SimpleMDE placeholder='Description' {...field} />
           )}
         />
-        {errors.description && <Text color='red' as='p'>{errors.description.message}</Text>}
+         <ErrorMessage>{errors.description?.message}</ErrorMessage>
         <Button>Submit new issue</Button>
       </form>
     </div>
