03348ba3e0de202139132f9be873973ba48e2bec
vattemabhishek <vattemabhishek@gmail.com>
2024-04-29 16:10:02 +0530

    build edit issue page

--- app/issues/[id]/IssueDetails.tsx
+++ app/issues/[id]/IssueDetails.tsx
@@ -1,15 +1,15 @@
 import { IssueStatusBadge } from '@/app/components'
 import { Issue } from '@prisma/client'
-import { Heading, Flex, Card, Text } from '@radix-ui/themes'
-import React from 'react'
+import { Card, Flex, Heading, Text } from '@radix-ui/themes'
 import ReactMarkdown from 'react-markdown'
 
 const IssueDetails = ({issue}:{issue:Issue}) => {
   return (
-     <><Heading as='h1'>{issue.title}</Heading><Flex gap='3' my='2'>
+     <><Heading as='h1'>{issue.title}</Heading>
+     <Flex gap='3' my='2'>
     <IssueStatusBadge status={issue.status} />
     <Text>{issue.createdAt.toDateString()}</Text>
-   </Flex><Card className='prose' mt='2'>
+    </Flex><Card className='prose' mt='2'>
      <ReactMarkdown>{issue.description}</ReactMarkdown>
     </Card></>
   )
--- app/issues/new/page.tsx
+++ app/issues/new/page.tsx
@@ -1,73 +1,9 @@
-'use client'
-import { ErrorMessage, Spinner } from '@/app/components'
-import { createIssueSchema } from '@/app/createIssueSchema'
-import { zodResolver } from '@hookform/resolvers/zod'
-import { Button, Callout, TextField } from '@radix-ui/themes'
-import axios from 'axios'
-import 'easymde/dist/easymde.min.css'
-import dynamic from 'next/dynamic'
-import { useRouter } from 'next/navigation'
-import { useState } from 'react'
-import { Controller, useForm } from 'react-hook-form'
-import { z } from 'zod'
+import IssueForm from '../_components/IssueForm'
 
-
-const SimpleMDE = dynamic(()=> import('react-simplemde-editor'),{ssr : false})
-
-
-type IssueForm = z.infer<typeof createIssueSchema>
-
-const NewIssuePage= () => {
- 
- const [error,setError] = useState("");
- const [isSubmitting,  setSubmitting]  =  useState(false);
-  const router = useRouter();
-  const {register, control, handleSubmit, formState: {errors}} = useForm<IssueForm>({
-   resolver  : zodResolver(createIssueSchema)
-});
-
-const onSubmit = handleSubmit(async (data) => {
-  try {
-    setSubmitting(true)
-    await axios.post('/api/issues', data)
-    router.push('/issues')
-  } catch (error) {
-    setSubmitting(false)
-    setError('An unexpected error occured')
-  }
-});
-
-
+const NewIssuePage = () => {
   return (
-     
-    <div className='max-w-xl  space-y-3'>
-      {error && (
-        <Callout.Root color='red'>
-          <Callout.Text>{error}</Callout.Text>
-        </Callout.Root>
-      )}
-      <form
-        className='max-w-xl space-y-3 '
-        onSubmit={onSubmit}
-      >
-        <TextField.Root>
-          <TextField.Input placeholder='Title' {...register('title')} />
-        </TextField.Root>
-      <ErrorMessage>{errors.title?.message}</ErrorMessage>
-        <Controller
-          name='description'
-          control={control}
-          render={({ field }) => (
-            <SimpleMDE placeholder='Description' {...field} />
-          )}
-        />
-         <ErrorMessage>{errors.description?.message}</ErrorMessage>
-        <Button disabled ={isSubmitting}>Submit new issue {isSubmitting && <Spinner/>}</Button>
-      </form>
-    </div>
+    <IssueForm/>
   )
 }
 
 export default NewIssuePage
-
-
