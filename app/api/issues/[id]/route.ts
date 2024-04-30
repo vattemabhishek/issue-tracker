d10584bfc28999bb4c3c80aa539ac88d4a36e76d
vattemabhishek <vattemabhishek@gmail.com>
2024-04-30 11:38:35 +0530

    build an api for deletion of isues

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -30,3 +30,23 @@ export async function PATCH(
 
 
 }
+
+export async function DELETE(
+ request: NextRequest,
+ {params}:{params:{id:string}}){
+ 
+  const issue = await prisma.issue.findUnique({
+    where : {id : parseInt(params.id)}
+  })
+
+    if(!issue)
+      return NextResponse.json({error:'Invalid issue'},{status : 404})
+
+    await prisma.issue.delete({
+      where : {id :issue.id}
+    });
+
+    return NextResponse.json({});
+
+}
+
