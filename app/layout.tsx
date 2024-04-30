9308430393446458c99e695cbe6e2b8aebd1fb73
vattemabhishek <vattemabhishek@gmail.com>
2024-04-30 18:21:14 +0530

    setup react query

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,11 +1,12 @@
+import { Container, Theme } from '@radix-ui/themes';
 import '@radix-ui/themes/styles.css';
-import './theme-config.css';
-import './globals.css'
-import type { Metadata } from 'next'
-import { Inter } from 'next/font/google'
-import NavBar from './NavBar'
-import { Container, Theme, ThemePanel } from '@radix-ui/themes'
+import QueryClientProvider from './QueryClientProvider';
+import type { Metadata } from 'next';
+import { Inter } from 'next/font/google';
 import AuthProvider from './auth/Provider';
+import './globals.css';
+import NavBar from './NavBar';
+import './theme-config.css';
 
 const inter = Inter({
  subsets: ['latin'],
@@ -24,14 +25,16 @@ export default function RootLayout({
   return (
     <html lang='en'>
       <body className={inter.variable}>
-        <AuthProvider>
-        <Theme appearance='light' accentColor='cyan' radius='full'>
-          <NavBar />
-          <main className='p-5'>
-            <Container>{children}</Container>
-          </main>
-        </Theme>
-        </AuthProvider>
+        <QueryClientProvider>
+          <AuthProvider>
+            <Theme appearance='light' accentColor='cyan' radius='full'>
+              <NavBar />
+              <main className='p-5'>
+                <Container>{children}</Container>
+              </main>
+            </Theme>
+          </AuthProvider>
+        </QueryClientProvider>
       </body>
     </html>
   )
