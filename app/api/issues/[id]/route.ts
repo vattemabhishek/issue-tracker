bf1a9c42cef463a32b56b6dbc5c56101ab64d12a
vattemabhishek <vattemabhishek@gmail.com>
2024-04-30 16:31:52 +0530

    secure the application by protcting the end points for update and delete

--- app/api/issues/[id]/route.ts
+++ app/api/issues/[id]/route.ts
@@ -1,10 +1,14 @@
+import authOptions from "@/app/auth/authOptions";
 import { issueSchema } from "@/app/validationSchema";
 import prisma from "@/prisma/client";
+import { getServerSession } from "next-auth";
 import { NextRequest, NextResponse } from "next/server";
 
 export async function PATCH(
  request: NextRequest,
  {params}:{params:{id:string}}){
+  const session = await getServerSession(authOptions)
+  if (!session) return NextResponse.json({}, { status: 401 })
 
    const body = await request.json();
 
@@ -35,6 +39,8 @@ export async function DELETE(
  request: NextRequest,
  {params}:{params:{id:string}}){
   
+  const session = await getServerSession(authOptions)
+  if (!session) return NextResponse.json({}, { status: 401 })
   
   const issue = await prisma.issue.findUnique({
     where : {id : parseInt(params.id)}
--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -4,10 +4,13 @@ import { notFound } from 'next/navigation';
 import EditIssueButton from './EditIssueButton';
 import IssueDetails from './IssueDetails';
 import DeleteIssueButton from './DeleteIssueButton';
+import { getServerSession } from 'next-auth';
+import authOptions from '@/app/auth/authOptions';
 
 
 
 const IssueDetailPage = async ({params} : {params: {id: string}}) => {
+ const session= await getServerSession(authOptions);
 
   const issue=await prisma.issue.findUnique({
    where:{ id : parseInt(params.id)}
@@ -21,12 +24,12 @@ const IssueDetailPage = async ({params} : {params: {id: string}}) => {
       <Box className='md:col-span-4'>
         <IssueDetails issue={issue} />
       </Box>
-      <Box>
+      {session && <Box>
         <Flex direction={'column'} gap={'4'}>
           <EditIssueButton issueId={issue.id} />
           <DeleteIssueButton issueId={issue.id} />
         </Flex>
-      </Box>
+      </Box>}
     </Grid>
   )
 }
