b5bd57620e20a5d21abd8cc76f352f7d3b6e42fd
vattemabhishek <vattemabhishek@gmail.com>
2024-05-01 22:00:08 +0530

    ad metadata

--- app/issues/[id]/page.tsx
+++ app/issues/[id]/page.tsx
@@ -1,39 +1,53 @@
-import prisma from '@/prisma/client';
-import { Box, Flex, Grid } from '@radix-ui/themes';
-import { notFound } from 'next/navigation';
-import EditIssueButton from './EditIssueButton';
-import IssueDetails from './IssueDetails';
-import DeleteIssueButton from './DeleteIssueButton';
-import { getServerSession } from 'next-auth';
-import authOptions from '@/app/auth/authOptions';
-import AssigneeSelect from './AssigneeSelect';
+import prisma from '@/prisma/client'
+import { Box, Flex, Grid } from '@radix-ui/themes'
+import { notFound } from 'next/navigation'
+import EditIssueButton from './EditIssueButton'
+import IssueDetails from './IssueDetails'
+import DeleteIssueButton from './DeleteIssueButton'
+import { getServerSession } from 'next-auth'
+import authOptions from '@/app/auth/authOptions'
+import AssigneeSelect from './AssigneeSelect'
+import { Metadata } from 'next'
 
+interface Props {
+  params: { id: string }
+}
 
+const IssueDetailPage = async ({ params }: Props) => {
+  const session = await getServerSession(authOptions)
 
-const IssueDetailPage = async ({params} : {params: {id: string}}) => {
- const session= await getServerSession(authOptions);
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  })
+  if (!issue) notFound()
 
-  const issue=await prisma.issue.findUnique({
-   where:{ id : parseInt(params.id)}
-  });
-  if(!issue)
-   notFound();
-
-    
   return (
     <Grid columns={{ initial: '1', sm: '5' }} gap='5'>
       <Box className='md:col-span-4'>
         <IssueDetails issue={issue} />
       </Box>
-      {session && <Box>
-        <Flex direction={'column'} gap={'4'}>
-          <AssigneeSelect issue={issue}/>
-          <EditIssueButton issueId={issue.id} />
-          <DeleteIssueButton issueId={issue.id} />
-        </Flex>
-      </Box>}
+      {session && (
+        <Box>
+          <Flex direction={'column'} gap={'4'}>
+            <AssigneeSelect issue={issue} />
+            <EditIssueButton issueId={issue.id} />
+            <DeleteIssueButton issueId={issue.id} />
+          </Flex>
+        </Box>
+      )}
     </Grid>
   )
+}
+
+export async function generateMetadata({ params }: Props) {
+  const issue = await prisma.issue.findUnique({
+    where: { id: parseInt(params.id) },
+  })
+
+  return {
+    title: issue?.title,
+    description: 'Details of issue' + issue?.id,
+  }
 }
 
 export default IssueDetailPage
